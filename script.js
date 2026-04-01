48be5739d882c5c702a82207b369f145f6d4288c
Filipe Figueiredo <filipe-camargo-figueiredo@hotmail.comm>
2026-04-01 11:33:00 -0300

    Finish Rock Paper Scissors game (console version)

---
+++ script.js
@@ -0,0 +1,65 @@
+console.log("Hello World");
+
+function getComputerChoice() {
+  const random = Math.random();
+
+  if (random < 0.33) {
+    return "rock";
+  } else if (random < 0.66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+  return prompt("Rock, Paper or Scissors?");
+}
+
+console.log(getHumanChoice());
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+      console.log("It's a tie!");
+      return;
+    }
+
+    if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+      humanScore++;
+    } else {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      computerScore++;
+    }
+  }
+
+  // 5 rounds
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+  playRound(getHumanChoice(), getComputerChoice());
+
+  // resultado final
+  if (humanScore > computerScore) {
+    console.log("You won the game!");
+  } else if (humanScore < computerScore) {
+    console.log("You lost the game!");
+  } else {
+    console.log("It's a tie game!");
+  }
+}
+
+playGame();
